03b0b3b09e4521c22f47724a28eaa0345310b4cb
unknown <rjkaz@comcast.net>
2022-03-27 22:42:29 -0400

    update

--- algorithms/binarySearches.js
+++ algorithms/binarySearches.js
@@ -1293,7 +1293,7 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch(testArray, 6));
-*/
+
 
 function binarySearch(arr, target) {
   let start = 0; 
@@ -1307,6 +1307,26 @@ function binarySearch(arr, target) {
       end = middle - 1; 
     } else if (arr[middle] === target) {
       return middle; 
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(testArray, 6));
+*/
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length -1;
+
+  while (start <= end) {
+    middle = Math.floor((start + end) / 2);
+    if (arr[middle] < target) {
+      start = middle + 1;
+    } else if (arr[middle] > target) {
+      end = middle -1;
+    } else if (arr[middle] === target){
+      return middle;
     }
   }
   return -1;
