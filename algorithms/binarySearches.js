6eb00ff091522f7820bcbf772aa5054827cf1b0e
unknown <rjkaz@comcast.net>
2022-04-05 22:20:51 -0400

    endless practice

--- algorithms/binarySearches.js
+++ algorithms/binarySearches.js
@@ -1431,7 +1431,7 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch(testArray, 6))
-*/
+
 
 function binarySearch(arr, target) {
   let start = 0;
@@ -1441,6 +1441,26 @@ function binarySearch(arr, target) {
     middle = Math.floor((start + end) / 2);
     if (arr[middle] < target) {
       start = middle +1;
+    } else if (arr[middle] > target) {
+      end = middle -1;
+    } else if (arr[middle] === target) {
+      return middle;
+    }
+  }
+  return -1;
+}
+
+console.log(binarySearch(testArray, 6))
+*/
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length -1;
+
+  while (start <= end) {
+    middle = Math.floor((start + end) / 2);
+    if(arr[middle] < target) {
+      start = middle + 1;
     } else if (arr[middle] > target) {
       end = middle -1;
     } else if (arr[middle] === target) {
