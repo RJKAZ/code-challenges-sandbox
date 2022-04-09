989ee0b81fff0965fdfe194b15e91e62c6a6293b
reese <rjkaz@comcast.net>
2022-04-09 14:20:01 -0400

    more practice

--- algorithms/binarySearches.js
+++ algorithms/binarySearches.js
@@ -1511,7 +1511,7 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch(testArray, 6))
-*/
+
 
 function binarySearch(arr, target) {
   let start = 0;
@@ -1531,3 +1531,21 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch(testArray, 6))
+*/
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    middle = Math.floor((start + end) / 2);
+    if (arr[middle] < target) {
+      start = middle + 1;
+    } else if (arr[middle] > target) {
+      end = middle - 1;
+    } else if (arr[middle] === target) {
+      return middle;
+    }
+  }
+  return -1;
+}
