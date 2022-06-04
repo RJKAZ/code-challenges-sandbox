30b2c7d938d956f4b6212950fe2aa05dc03a8428
unknown <rjkaz@comcast.net>
2022-06-03 22:02:39 -0400

    update

--- algorithms/binarySearches.js
+++ algorithms/binarySearches.js
@@ -2497,6 +2497,25 @@ function binarySearch(arr, target) {
   return -1;
 }
 console.log(binarySearch(testArray, 6));
+
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    middle = Math.floor((start + end) / 2);
+    if (arr[middle] < target) {
+      start = middle + 1;
+    } else if (arr[middle] > target) {
+      end = middle - 1;
+    } else if (arr[middle] === target) {
+      return middle;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch(testArray, 6));
+
 */
 function binarySearch(arr, target) {
   let start = 0;
